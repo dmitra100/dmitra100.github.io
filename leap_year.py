91e746a1a8e7126efd94bc98ce8fcebd9abfc304
debasish <d.mitra100@gmail.com>
2018-08-01 15:50:35 +0530

    Program to find leap year

---
+++ leap_year.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Program to find leap year
+# Logic year should be divided by 4, 400 and year which are divisible by 100 is not leap year.
+#1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years
+
+def is_leap(year):
+    if year % 400 ==0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4==0:
+        return True
+    else:
+        return False
+year = int(raw_input())
+print is_leap(year)
